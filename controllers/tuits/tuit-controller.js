d6147a45364da10e40c726ec6b66b2aad51b2b0a
Stephen Yu <stephenyu1229@gmail.com>
2023-04-02 00:54:26 -0400

    first commit and build api for tuiter

---
+++ controllers/tuits/tuit-controller.js
@@ -0,0 +1,39 @@
+import posts from "./tuits.js";
+
+let tuits = posts;
+
+const createTuit = (req, res) => {
+  const newTuit = req.body;
+  newTuit._id = new Date().getTime() + "";
+  newTuit.likes = 0;
+  newTuit.liked = false;
+  newTuit.dislikes = 0;
+  newTuit.disliked = false;
+  tuits.unshift(newTuit);
+  res.json(newTuit);
+};
+
+const findTuits = (req, res) => {
+  res.json(tuits);
+};
+
+const updateTuit = (req, res) => {
+  const index = req.params.tid;
+  const updates = req.body;
+  const tuitIndex = tuits.findIndex((t) => t._id === index);
+  tuits[tuitIndex] = { ...tuits[tuitIndex], ...updates };
+  res.sendStatus(200);
+};
+
+const deleteTuit = (req, res) => {
+  const index = req.params.tid;
+  tuits = tuits.filter((tuit) => tuit._id !== index);
+  res.sendStatus(200);
+};
+
+export default (app) => {
+  app.post("/api/tuits", createTuit);
+  app.get("/api/tuits", findTuits);
+  app.put("/api/tuits/:tid", updateTuit);
+  app.delete("/api/tuits/:tid", deleteTuit);
+};
