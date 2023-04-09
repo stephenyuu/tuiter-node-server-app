4e16749171939de5f8c214a37ddb8c1870b3c99f
Stephen Yu <stephenyu1229@gmail.com>
2023-04-09 19:27:06 -0400

    delete debug console log

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ import TuitsController from "./controllers/tuits/tuit-controller.js";
 
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
-console.log(CONNECTION_STRING);
-  mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
