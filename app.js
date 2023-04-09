b390bfd654d1882f30085e7bb7d54c48dfd3ef5f
Stephen Yu <stephenyu1229@gmail.com>
2023-04-09 19:07:04 -0400

    add console log to debug connection string issue

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import TuitsController from "./controllers/tuits/tuit-controller.js";
 
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
-mongoose.connect(CONNECTION_STRING);
+console.log(CONNECTION_STRING);
+  mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
